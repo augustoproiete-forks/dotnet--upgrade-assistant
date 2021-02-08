5cc0c4fb0ed6a6242cc54255301f13a222a02622
Taylor Southwick <tasou@microsoft.com>
2021-02-08 11:29:02 -0800

    Code changes

--- src/Analyzers/AspNetMigrator.Analyzers.CodeFixes/IdentifierMigrationCodeFixer.cs
+++ src/Analyzers/AspNetMigrator.Analyzers.CodeFixes/IdentifierMigrationCodeFixer.cs
@@ -24,32 +24,40 @@ namespace AspNetMigrator.Analyzers
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            if (root is null)
+            {
+                return;
+            }
+
             var node = root.FindNode(context.Span);
 
-            if (node == null)
+            if (node is null)
             {
                 return;
             }
 
             var diagnostic = context.Diagnostics.FirstOrDefault();
-            if (diagnostic == null)
+
+            if (diagnostic is null)
             {
                 return;
             }
 
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    CodeFixTitle,
-                    cancellationToken => UpdateIdentifierTypeAsync(context.Document, node, diagnostic.Properties[IdentifierMigrationAnalyzer.NewIdentifierKey], cancellationToken),
-                    CodeFixTitle),
-                context.Diagnostics);
+            if (diagnostic.Properties.TryGetValue(IdentifierMigrationAnalyzer.NewIdentifierKey, out var property) && property is not null)
+            {
+                // Register a code action that will invoke the fix.
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        CodeFixTitle,
+                        cancellationToken => UpdateIdentifierTypeAsync(context.Document, node, property, cancellationToken),
+                        CodeFixTitle),
+                    context.Diagnostics);
+            }
         }
 
         private static async Task<Document> UpdateIdentifierTypeAsync(Document document, SyntaxNode node, string newIdentifier, CancellationToken cancellationToken)
         {
-            var editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
-
             var name = node switch
             {
                 // The most common case is that the node is an IdentifierName
@@ -102,16 +110,18 @@ namespace AspNetMigrator.Analyzers
 
             // Work on the document root instead of updating nodes with the editor directly so that
             // additional changes can be made later (adding using statement) if necessary.
-            var documentRoot = editor.OriginalRoot as CompilationUnitSyntax;
+            if (await document.GetSyntaxRootAsync(cancellationToken) is not CompilationUnitSyntax documentRoot)
+            {
+                return document;
+            }
+
             documentRoot = documentRoot.ReplaceNode(name, updatedName)!;
 
             // Add using declation if needed
             var namespaceName = GetNamespace(newIdentifier);
             documentRoot = documentRoot.AddUsingIfMissing(namespaceName);
 
-            editor.ReplaceNode(editor.OriginalRoot, documentRoot);
-
-            return editor.GetChangedDocument();
+            return document.WithSyntaxRoot(documentRoot);
         }
 
         private static string GetNamespace(string newIdentifier) => newIdentifier.Substring(0, newIdentifier.LastIndexOf('.'));
